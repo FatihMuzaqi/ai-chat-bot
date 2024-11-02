296ec99103e3da76b4504ff8ddf451e3a71bf78d
FatihMuzaqi <fatih.muzaqi123@gmail.com>
2024-11-02 14:14:47 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+function App() {
+
+  return (
+    <>
+      <div>
+          <h1>HALO WORLD</h1>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aliquid animi nam sunt ipsum eius beatae a esse natus vero iusto unde error tempora, numquam excepturi itaque nostrum cumque repudiandae maiores.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
